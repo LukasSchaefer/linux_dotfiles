d9ed50c1fcbb056ef860a4d9414890d56da127a3
Lukas Schaefer <luki.schaefer96@gmail.com>
2020-11-17 13:03:42 +0100

    update volumeControl script to get values from pulseaudio (nice even numbers also matching polybar module)

--- scripts/system_status/volumeControl.sh
+++ scripts/system_status/volumeControl.sh
@@ -8,28 +8,35 @@
 # Script modified from these wonderful people:
 # https://github.com/dastorm/volume-notification-dunst/blob/master/volume.sh
 # https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a
+# pacmd commands from https://unix.stackexchange.com/questions/132230/read-out-pulseaudio-volume-from-commandline-i-want-pactl-get-sink-volume
 
 function get_volume {
-  amixer -M get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
+    pacmd list-sinks|grep -A 15 '* index'| awk '/volume: front/{ print $5 }' | sed 's/[%|,]//g'
 }
 
 function is_mute {
-  amixer -M get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
+    muted=$(pacmd list-sinks|grep -A 15 '* index'|awk '/muted:/{ print $2 }')
+    if [ $muted == "no" ]; then
+        false
+    else
+        true
+    fi
 }
 
 function send_notification {
-  iconSound="audio-volume-high"
-  iconMuted="audio-volume-muted"
-  if is_mute ; then
-    dunstify -t 1000 -i $iconMuted -r 2593 -u normal "mute"
-  else
-    volume=$(get_volume)
-    # Make the bar with the special character ─ (it's not dash -)
-    # https://en.wikipedia.org/wiki/Box-drawing_character
-    bar=$(seq --separator="─" 0 "$((volume / 5))" | sed 's/[0-9]//g')
-    # Send the notification
-    dunstify -t 1000 -i $iconSound -r 2593 -u normal "$volume%"$'\n'"$bar"
-  fi
+    iconSound="audio-volume-high"
+    iconMuted="audio-volume-muted"
+
+    if is_mute; then
+        dunstify -t 1000 -i $iconMuted -r 2593 -u normal "mute"
+    else
+        volume=$(get_volume)
+        # Make the bar with the special character ─ (it's not dash -)
+        # https://en.wikipedia.org/wiki/Box-drawing_character
+        bar=$(seq --separator="─" 0 "$((volume / 5))" | sed 's/[0-9]//g')
+        # Send the notification
+        dunstify -t 1000 -i $iconSound -r 2593 -u normal "$volume%"$'\n'"$bar"
+    fi
 }
 
 case $1 in
