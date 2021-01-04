a930d7bbdae530a45600a40baaeeb884302803c6
Lukas Schaefer <luki.schaefer96@gmail.com>
2021-01-04 13:58:41 +0100

    fix screenlayouts and polybar monitor config for dual screen with on-demand nvidia prime (other monitor names) and add GPU util/temp to polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,14 +9,15 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
     DP_connected="none"
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        if [ "$m" != "eDP-1-1" ]; then
+        if [[ $m != "eDP"* ]] ; then
+            echo $m
             DP_connected=$m
         fi
     done
     if [ "$DP_connected" != "none" ]; then
         echo "Start polybar on eDP-1-1 and $DP_connected"
         polybar $DP_connected &
-        polybar eDP-1-1 &
+        polybar eDP-1 &
     else
         polybar --reload master &
     fi
--- screenlayouts/dual_screen_left.sh
+++ screenlayouts/dual_screen_left.sh
@@ -3,4 +3,4 @@ if [ $# != 1 ]; then
     echo "Usage: ./dual_screen_left.sh <screen_name>"
     exit 1
 fi
-xrandr --output eDP-1-1 --mode 1920x1200 --pos 2560x0 --rotate normal --output $1 --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --mode 1920x1200 --pos 2560x240 --rotate normal --output $1 --mode 2560x1440 --pos 0x0 --rotate normal
