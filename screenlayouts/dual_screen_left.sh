1c22e405d2696524c30b2093dacf5f823e7d4c05
Lukas Schaefer <luki.schaefer96@gmail.com>
2021-01-16 20:33:12 +0100

    edit dual screenlayouts starting script

--- screenlayouts/dual_screen_left.sh
+++ screenlayouts/dual_screen_left.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ $# != 1 ]; then
-    echo "Usage: ./dual_screen_left.sh <screen_name>"
+if [ $# != 2 ]; then
+    echo "Usage: ./dual_screen_left.sh <eDP_name> <DP_name>"
     exit 1
 fi
-xrandr --output eDP-1 --mode 1920x1200 --pos 2560x240 --rotate normal --output $1 --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output $1 --mode 1920x1200 --pos 2560x240 --rotate normal --output $2 --mode 2560x1440 --pos 0x0 --rotate normal
--- screenlayouts/start_dual.sh
+++ screenlayouts/start_dual.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 DP_connected="none"
-for m in $(/usr/bin/xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [ "$m" != "eDP-1-1" ]; then
+eDP_connected="none"
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [[ $m != "eDP"* ]] ; then
+        echo $m
         DP_connected=$m
+    else
+        eDP_connected=$m
     fi
 done
 if [ "$DP_connected" != "none" ]; then
     echo "Found connected screen at $DP_connected"
-    bash ~/.screenlayout/dual_screen_left.sh $DP_connected
+    bash ~/.screenlayout/dual_screen_left.sh $eDP_connected $DP_connected
+else
+    echo "No external screens found"
+    xrandr --output $eDP_connected --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
 fi
