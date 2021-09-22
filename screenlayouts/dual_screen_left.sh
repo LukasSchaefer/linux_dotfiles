d86098fb07fd1b3c5a8a8534e8906f07d0582e36
Lukas Schaefer <luki.schaefer96@gmail.com>
2021-09-22 10:28:03 +0100

    screenlayout detection for dual screen start robust to varying resolution

--- screenlayouts/dual_screen_left.sh
+++ screenlayouts/dual_screen_left.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-if [ $# != 2 ]; then
-    echo "Usage: ./dual_screen_left.sh <eDP_name> <DP_name>"
+if [ $# != 3 ]; then
+    echo "Usage: ./dual_screen_left.sh <eDP_name> <DP_name> <DP_resolution>"
     exit 1
 fi
-xrandr --output $1 --mode 1920x1200 --pos 2560x240 --rotate normal --output $2 --mode 2560x1440 --pos 0x0 --rotate normal
+if [[ $3 == 1920* ]]; then
+    pos="1920x0"
+else
+    pos="2560x240"
+fi
+xrandr --output $1 --mode 1920x1200 --pos $pos --rotate normal --output $2 --mode $3 --pos 0x0 --rotate normal
--- screenlayouts/start_dual.sh
+++ screenlayouts/start_dual.sh
@@ -1,17 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 DP_connected="none"
+DP_resolution="none"
 eDP_connected="none"
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     if [[ $m != "eDP"* ]] ; then
-        echo $m
         DP_connected=$m
+        DP_resolution=$(xrandr -q | grep 'DP-2' | awk '{print $3}' | sed 's/+.*//')
     else
         eDP_connected=$m
     fi
 done
 if [ "$DP_connected" != "none" ]; then
-    echo "Found connected screen at $DP_connected"
-    bash ~/.screenlayout/dual_screen_left.sh $eDP_connected $DP_connected
+    echo "Internal screen at $eDP_connected"
+    echo "Found connected screen at $DP_connected with resolution $DP_resolution"
+    bash ~/.screenlayout/dual_screen_left.sh $eDP_connected $DP_connected $DP_resolution
 else
     echo "No external screens found"
     xrandr --output $eDP_connected --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
