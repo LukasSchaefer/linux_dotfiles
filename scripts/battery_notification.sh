231f0e95fb403bc0b4df784eef5c225832108ee2
Lukas Schaefer <luki.schaefer96@gmail.com>
2022-03-26 22:44:41 +0000

    update battery notification script

--- scripts/battery_notification.sh
+++ scripts/battery_notification.sh
@@ -10,7 +10,7 @@
 # export $(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME gnome-session | head -n 1)/environ | tr '\0' '\n')
 
 BATTERY_PATH=$(upower -e | grep battery)
-LINE_POWER_PATH=$(upower -e | grep line_power)
+LINE_POWER_PATH=$(upower -e | grep line_power_AC)
 BATTERY_PERCENTAGE=$(upower -i $BATTERY_PATH | grep 'percentage:' | awk '{ print $2 }' | sed 's/%//')
 CABLE_PLUGGED=$(upower -i $LINE_POWER_PATH | grep -A2 'line-power' | grep online | awk '{ print $2 }')
 
@@ -22,11 +22,13 @@ if [[ $CABLE_PLUGGED == 'yes' ]]; then
 
 else
 
-    if [[ $BATTERY_PERCENTAGE -lt 20 ]]; then
-        /usr/local/bin/dunstify --urgency=normal -i "battery-low" "Battery notification" "Battery is below 20%, plug in the power cable to optimize battery life."
+    if [[ $BATTERY_PERCENTAGE -lt 5 ]]; then
+        notify-send --urgency=critical -i "battery-empty" "Battery notification" "Battery is below 5%!!!"
     elif [[ $BATTERY_PERCENTAGE -lt 10 ]]; then
-        /usr/local/bin/dunstify --urgency=critical -i "battery-low" "Battery notification" "Battery is below 10%, plug in the power cable!"
-    elif [[ $BATTERY_PERCENTAGE -lt 5 ]]; then
-        /usr/local/bin/dunstify --urgency=critical -i "battery-empty" "Battery notification" "Battery is below 5%, plug in the power cable now!"
+        notify-send --urgency=critical -i "battery-low" "Battery notification" "Battery is below 10%!"
+    elif [[ $BATTERY_PERCENTAGE -lt 20 ]]; then
+        notify-send --urgency=normal -i "battery-low" "Battery notification" "Battery is below 20%."
+    elif [[ $BATTERY_PERCENTAGE -lt 50 ]]; then
+        notify-send --urgency=normal -i "battery-good" "Battery notification" "Battery is below 50%."
     fi
 fi
