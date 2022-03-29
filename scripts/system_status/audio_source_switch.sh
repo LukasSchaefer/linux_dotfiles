25c8837501dea356ef52b476c0e8cfd9f01e4f81
Lukas Schaefer <luki.schaefer96@gmail.com>
2022-03-29 11:56:40 +0100

    fix audio source switch script to avoid infinite looping previously unfound source led to infinite looping

--- scripts/system_status/audio_source_switch.sh
+++ scripts/system_status/audio_source_switch.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 SOURCE_NAME=$(pacmd info | grep "Default sink name" | head -1 | awk '{print $4}')
-# check if Focusrite interface used or not
-if [[ "$SOURCE_NAME" == *"Focusrite_Scarlett_2i2"* ]]; then
-    # change sound to Schiit Modi
-    TARGET_NAME="Schiit_Audio_USB_Modi"
-elif [[ "$SOURCE_NAME" == *"Schiit_Audio_USB_Modi"* ]]; then
-    # change sound to built-in speaker
-    TARGET_NAME="pci-0000"
-elif [[ "$SOURCE_NAME" == *"pci-0000"* ]]; then
-    # change sound to audio interface
-    TARGET_NAME="Focusrite_Scarlett_2i2"
-else
-    # default otherwise change sound to built-in speaker
-    TARGET_NAME="pci-0000"
-fi
+NUM_SOURCES=$(pacmd list-sinks | grep "index:" | tail -1 | grep -Eo '[0-9]+$')
 
-i=0
-while [[ "$SOURCE_NAME" != *$TARGET_NAME* ]]
+CHANGED=0
+
+while [[ $CHANGED -ne 1 ]]
 do
-    pacmd set-default-sink $i
-    SOURCE_NAME=$(pacmd info | grep "Default sink name" | head -1 | awk '{print $4}')
-    echo "$i: $SOURCE_NAME --> $TARGET_NAME"
-    i=$[$i+1]
+    # check if Focusrite interface used or not
+    if [[ "$SOURCE_NAME" == *"Focusrite_Scarlett_2i2"* ]]; then
+        # change sound to Schiit Modi
+        TARGET_NAME="Schiit_Audio_USB_Modi"
+    elif [[ "$SOURCE_NAME" == *"Schiit_Audio_USB_Modi"* ]]; then
+        # change sound to built-in speaker
+        TARGET_NAME="pci-0000"
+    elif [[ "$SOURCE_NAME" == *"pci-0000"* ]]; then
+        # change sound to audio interface
+        TARGET_NAME="Focusrite_Scarlett_2i2"
+    else
+        # default otherwise change sound to built-in speaker
+        TARGET_NAME="pci-0000"
+    fi
+    echo "Find sink for $TARGET_NAME"
+
+    for i in $(seq 0 $NUM_SOURCES);
+    do
+        OUTPUT=$(pacmd set-default-sink $i)
+        SOURCE_NAME=$(pacmd info | grep "Default sink name" | head -1 | awk '{print $4}')
+        if [[ "$SOURCE_NAME" == *"$TARGET_NAME"* ]]; then
+            echo "Found at sink $i: $OUTPUT"
+            CHANGED=1
+            break
+        fi
+    done
+    SOURCE_NAME=TARGET_NAME
 done
